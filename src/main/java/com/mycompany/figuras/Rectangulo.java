8d9b461087884fada748b6e43943655fdf976b92
Felipe <juanfel2024@gmail.com>
2020-05-19 19:37:45 +0200

    Creación de la clase Rectangulo y el metodo obtenerArea

---
+++ src/main/java/com/mycompany/figuras/Rectangulo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author alumno
+ */
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double lado1, double lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+}
